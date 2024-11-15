431c1612f295b1d5c998ae29e364bcc876d27824
Ayodeji O <97124713+aosasona@users.noreply.github.com>
2024-11-15 01:14:11 +0000

    Added workaround 2

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,21 +1,9 @@
-use std::sync::Arc;
+use std::{fs, ops::Deref};
 
 use serde::{Deserialize, Serialize};
-use sqlx::sqlite::SqlitePool;
-use tauri::{AppHandle, Manager as _};
+use sqlx::SqlitePool;
+use tauri::Manager as _;
 use tauri_plugin_sql::Migration;
-
-const DB_NAME: &str = "todo.db";
-
-macro_rules! dsn {
-    () => {
-        format!("sqlite:{}", DB_NAME).as_str()
-    };
-}
-
-struct AppState {
-    db: Arc<SqlitePool>,
-}
 
 #[derive(sqlx::FromRow, Serialize, Deserialize)]
 struct Todo {
@@ -28,95 +16,118 @@ struct Todo {
 }
 
 #[tauri::command]
-async fn create_todo(state: tauri::State<'_, AppState>, data: Todo) -> Result<Todo, String> {
-    let db = state.db.clone();
-    let mut conn = db.acquire().await.unwrap();
-
-    let row = sqlx::query_as::<_, Todo>(
-        r#"
-        INSERT INTO todos (title, completed, created_at)
-        VALUES ($1, $2, $3)
-        RETURNING id, title, completed, created_at
-        "#,
+async fn create_todo(
+    db: tauri::State<'_, sqlx::sqlite::SqlitePool>,
+    title: String,
+) -> Result<Todo, String> {
+    let now = chrono::Utc::now().timestamp();
+    let todo = sqlx::query_as::<_, Todo>(
+        "INSERT INTO todos (title, created_at) VALUES (?, ?) RETURNING *",
     )
-    .bind(data.title)
-    .bind(data.completed)
-    .bind(chrono::Utc::now().timestamp())
-    .fetch_one(&mut *conn)
+    .bind(title)
+    .bind(now)
+    .fetch_one(db.deref())
     .await
     .map_err(|e| e.to_string())?;
-
-    Ok(row)
+    Ok(todo)
 }
 
 #[tauri::command]
-async fn get_all_todos(state: tauri::State<'_, AppState>) -> Result<Vec<Todo>, String> {
-    let db = state.db.clone();
-    let mut conn = db.acquire().await.unwrap();
-
-    let rows = sqlx::query_as::<_, Todo>(
-        r#"
-        SELECT id, title, completed, created_at
-        FROM todos
-        "#,
-    )
-    .fetch_all(&mut *conn)
-    .await
-    .map_err(|e| e.to_string())?;
-
-    Ok(rows)
+async fn delete_todo(
+    db: tauri::State<'_, sqlx::sqlite::SqlitePool>,
+    id: i64,
+) -> Result<(), String> {
+    sqlx::query("DELETE FROM todos WHERE id = ?")
+        .bind(id)
+        .execute(db.deref())
+        .await
+        .map_err(|e| e.to_string())?;
+    Ok(())
 }
 
-async fn setup(app: &AppHandle) {
-    log::info!("Setting up the app");
-    let raw_db_pool = SqlitePool::connect(dsn!())
+#[tauri::command]
+async fn toggle_completed(
+    db: tauri::State<'_, sqlx::sqlite::SqlitePool>,
+    id: i64,
+) -> Result<Todo, String> {
+    let todo = sqlx::query_as::<_, Todo>("SELECT * FROM todos WHERE id = ?")
+        .bind(id)
+        .fetch_one(db.deref())
         .await
-        .expect("Failed to initialize database");
-    let db_pool = Arc::new(raw_db_pool);
+        .map_err(|e| e.to_string())?;
 
-    log::info!("Setting up state management");
-    app.manage(AppState { db: db_pool });
+    let completed = !todo.completed;
+    sqlx::query("UPDATE todos SET completed = ? WHERE id = ?")
+        .bind(completed)
+        .bind(id)
+        .execute(db.deref())
+        .await
+        .map_err(|e| e.to_string())?;
+
+    Ok(Todo {
+        id: todo.id,
+        title: todo.title,
+        completed,
+        created_at: todo.created_at,
+    })
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations: Vec<Migration> = vec![Migration {
-        version: 1,
-        description: "create_todos_table",
-        sql: "CREATE TABLE IF NOT EXISTS todos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            title TEXT NOT NULL,
-            completed BOOLEAN NOT NULL DEFAULT 0,
-            created_at INTEGER NOT NULL
-        )",
-        kind: tauri_plugin_sql::MigrationKind::Up,
-    }];
-
-    // NOTE: uncomment to use `.manage`
-    // let raw_db_pool = tauri::async_runtime::block_on(async {
-    //     SqlitePool::connect(dsn!())
-    //         .await
-    //         .expect("Failed to initialize database")
-    // });
-    // let db_pool = Arc::new(raw_db_pool);
-
-    tauri::Builder::default()
-        // .manage(AppState { db: db_pool }) // NOTE: uncomment to use `.manage`
-        .setup(|app: &mut tauri::App| {
-            tauri::async_runtime::block_on(async {
-                setup(app.handle()).await;
-            });
-
-            Ok(())
-        })
+    let app = tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_log::Builder::new().build())
-        .plugin(tauri_plugin_shell::init())
         .plugin(
             tauri_plugin_sql::Builder::new()
-                .add_migrations(dsn!(), migrations)
+                .add_migrations(
+                    "sqlite:todos.db",
+                    vec![Migration {
+                        version: 1,
+                        description: "create_todos_table",
+                        sql: "
+                        CREATE TABLE IF NOT EXISTS todos (
+                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                            title TEXT NOT NULL,
+                            completed BOOLEAN NOT NULL DEFAULT 0,
+                            created_at INTEGER NOT NULL
+                        )",
+                        kind: tauri_plugin_sql::MigrationKind::Up,
+                    }],
+                )
                 .build(),
         )
-        .invoke_handler(tauri::generate_handler![create_todo, get_all_todos])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .plugin(tauri_plugin_shell::init())
+        .invoke_handler(tauri::generate_handler![
+            create_todo,
+            delete_todo,
+            toggle_completed
+        ])
+        .build(tauri::generate_context!())
+        .expect("error while building tauri application");
+
+    let app_path = app
+        .path()
+        .app_config_dir()
+        .expect("No App config path was found!");
+
+    let db_path = app_path.join("todos.db");
+    let db_path_str = format!("sqlite:{}", db_path.to_string_lossy());
+
+    let pool = tauri::async_runtime::block_on(async {
+        let db_dir = std::path::Path::new(&db_path).parent().unwrap();
+
+        // If the parent directory does not exist, create it.
+        if !db_dir.exists() {
+            fs::create_dir_all(db_dir).unwrap();
+        }
+
+        fs::File::create(db_path).expect("Failed to create database file");
+
+        SqlitePool::connect(&db_path_str)
+            .await
+            .expect("Failed to connect to database")
+    });
+
+    app.manage(pool);
+    app.run(|_, _| {});
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { useState } from "react";
+import * as log from "@tauri-apps/plugin-log";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
-import { warn, debug, trace, info, error } from '@tauri-apps/plugin-log';
+import "./App.css";
+import Database from "@tauri-apps/plugin-sql";
+import { Drawer } from 'vaul';
 
-function forwardConsole(
-	fnName: 'log' | 'debug' | 'info' | 'warn' | 'error',
-	logger: (message: string) => Promise<void>
-) {
-	const original = console[fnName];
-	console[fnName] = (message) => {
-		original(message);
-		logger(message);
-	};
+function MyComponent() {
+	return (
+		<Drawer.Root>
+			<Drawer.Trigger>Open</Drawer.Trigger>
+			<Drawer.Portal>
+				<Drawer.Content>
+					<Drawer.Title>Title</Drawer.Title>
+				</Drawer.Content>
+				<Drawer.Overlay />
+			</Drawer.Portal>
+		</Drawer.Root>
+	);
 }
-
-forwardConsole('log', trace);
-forwardConsole('debug', debug);
-forwardConsole('info', info);
-forwardConsole('warn', warn);
-forwardConsole('error', error);
 
 type Todo = {
 	id?: number;
@@ -26,13 +26,26 @@ type Todo = {
 	createdAt: number;
 }
 
+
 function App() {
 	const [todos, setTodos] = useState<Todo[]>([]);
+	const [db, setDb] = useState<Database | null>(null);
+
+	useEffect(() => {
+		Database.load("sqlite:todos.db")
+			.then((conn) => {
+				conn.select("SELECT * FROM todos")
+					.then((rows) => { setTodos(rows as Todo[]); })
+					.catch(log.error)
+					.finally(() => { setDb(conn); })
+			})
+			.catch(log.error)
+	}, [])
+
 
 	async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
 		try {
 			event.preventDefault();
-			event.stopPropagation();
 
 			const data = new FormData(event.currentTarget);
 			const title = data.get("title") as string;
@@ -44,26 +57,63 @@ function App() {
 		}
 	}
 
+	async function deleteTodo(id: number) {
+		try {
+			await invoke("delete_todo", { id });
+			setTodos(todos.filter(todo => todo.id !== id));
+		} catch (error) {
+			console.error(error);
+			window.alert((error as any)?.message || "An error occurred");
+		}
+	}
+
+	async function toggleTodoStatus(id: number) {
+		try {
+			const todo = todos.find(todo => todo.id === id);
+			if (!todo) return;
+
+			const result: Todo = await invoke("toggle_completed", { id });
+			setTodos(todos.map(todo => todo.id === id ? result : todo));
+		} catch (error) {
+			console.error(error);
+			window.alert((error as any)?.message || "An error occurred");
+		}
+	}
+
+	if (!db) {
+		return (
+			<main className="container mx-auto flex items-center justify-center h-screen">
+				<p className="text-sm text-center text-neutral-300">Loading...</p>
+			</main>
+		);
+	}
+
 	return (
-		<main className="container px-3">
-			<h2 className="text-2xl font-bold text-zinc-900">Todos</h2>
-			<form className="px-2 py-3 rounded-md space-y-3" onSubmit={handleFormSubmit}>
-				<input name="title" className="w-full border border-zinc-200 py-1 px-2 focus:outline-none focus:ring focus:ring-zinc-900" placeholder="Add a new todo" />
-				<button type="submit" className="w-full text-sm bg-zinc-900 text-white py-2 px-2 rounded-md hover:scale-95 hover:border hover:border-neutral-800 transition-all">Add</button>
+		<main className="container mx-auto px-3 mt-safe-top">
+			<div className="min-h-safe-top w-screen fixed top-0 left-0 bg-neutral-900/80 backdrop-blur-lg">
+			</div>
+			<h2 className="text-2xl font-bold text-neutral-100 mb-4">Tasks</h2>
+			<form className="space-y-3 mb-6" onSubmit={handleFormSubmit}>
+				<input name="title" className="w-full bg-neutral-800 text-white py-2 px-3 text-sm focus:outline-none focus:ring focus:ring-neutral-200 rounded-md" placeholder="Add a new todo" autoFocus />
+				<button type="submit" className="w-full text-sm bg-neutral-100 text-neutral-900 py-2 px-2 rounded-md hover:scale-95 hover:border hover:border-neutral-800 transition-all">Add</button>
 			</form>
 
 			{todos.length === 0 ? (
 				<div className="mt-3">
-					<p className="text-sm text-center text-zinc-500">No todos yet.</p>
+					<p className="text-sm text-center text-neutral-300">No todos yet.</p>
 				</div>
 			) : (
-				<ul className="mt-3 border border-zinc-200 rounded-md">
+				<ul className="mt-3 border border-neutral-800 rounded-md">
 					{todos.map(todo => (
-						<li key={todo.id} className="flex items-center justify-between px-2 py-1.5 border-b border-zinc-200 last:border-b-0">
+						<li key={todo.id} className="flex items-center justify-between px-2 py-1.5 border-b border-neutral-800 last:border-b-0">
 							<label className="flex items-center space-x-2">
-								<input type="checkbox" checked={todo.completed} />
-								<span className={todo.completed ? "line-through" : ""}>{todo.title}</span>
+								<input type="checkbox" checked={todo.completed} onChange={() => toggleTodoStatus(todo.id as number)} />
+								<span className={todo.completed ? "line-through opacity-60" : ""}>{todo.title}</span>
 							</label>
+
+							<button className="text-sm" onClick={() => deleteTodo(todo.id as number)}>
+								❌
+							</button>
 						</li>
 					))}
 				</ul>
